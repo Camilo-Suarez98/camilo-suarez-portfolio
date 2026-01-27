4323c3759d4fb5282e2bb35270583c5d08120561
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-27 17:10:06 -0500

    feat: create files to handle projects and expereiences

---
+++ lib/experiences.ts
@@ -0,0 +1,26 @@
+export const experiences = [
+  {
+    dateRange: "July 2025 — Present",
+    title: "Frontend Developer",
+    company: "Mocion Experience Tech",
+    description:
+      "I develop a new software to create events and receive the data from users about the event. And that data could be selled to other companies to make new events and improve the user experience.",
+    technologies: ["React", "TypeScript", "Node.js", "WordPress", "Mantine"],
+  },
+  {
+    dateRange: "January 2023 — Present",
+    title: "Frontend Developer",
+    company: "Freelance",
+    description:
+      "I have developed websites and landing pages for several US-based companies using different technologies, enhancing their online presence and showcasing expertise in web technologies and design trends and I work closely with a senior frontend developer at a major US company, playing a key role in identifying and resolving bugs in web projects and contributing to the creation of new interfaces, expanding frontend development experience.",
+    technologies: ["React", "Next.js", "Tailwind CSS", "JavaScript", "WordPress", "GHL", "WIX"],
+  },
+  {
+    dateRange: "July 2022 - December 2022",
+    title: "Frontend Developer",
+    company: "Genco",
+    description:
+      "Started my professional journey building the company's website. Contributed to various projects while developing strong foundations in software development principles.",
+    technologies: ["HTML", "CSS", "JavaScript", "React", "Tailwind CSS"],
+  },
+];
---
+++ lib/projects.ts
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    title: "Cine Explorer",
+    description:
+      "A platform to search movies with a list of popular movies and log in to choose your favorite movies and receive AI recommendations based on your choices.",
+    projectUrl: "https://cine-explorer-six.vercel.app",
+    technologies: ["Next.js", "TMDB", "OpenAI API", "Tailwind CSS", "NextAuth", "Vercel"],
+  },
+  {
+    title: "Be Kind Dashboard",
+    description:
+      "A dashboard to show categories like name, description logo and color, the endpoint is provided by a third party API.",
+    projectUrl: "https://rick-and-morty-graphql-mo.vercel.app",
+    technologies: ["React", "Tailwind CSS", "Vercel"],
+  },
+  {
+    title: "Explora Events",
+    description: "A project where you can see different events, name, date, type and filter by those items, then you can click each one to see more details about the event.",
+    projectUrl: "https://explora-eventos.vercel.app",
+    technologies: ["React", "Tailwind CSS", "Zustand", "Vercel"],
+  },
+  {
+    title: "Rick and Morty",
+    description:
+      "A project to see all characters from famous Rick and Morty series, with filters by name, status and gender. The endpoint is provided by a third party API.",
+    projectUrl: "https://rick-and-morty-murex-sigma.vercel.app",
+    technologies: ["Next.js", "Tailwind CSS", "Rick and Morty API", "Vercel"],
+  },
+  {
+    title: "Pokemon App",
+    description: "This is a page about Pokémon, using the PokeApi to fetch the first generation of the popular TV Show, you can see them in a table view or grid and click each one to see more details.",
+    projectUrl: "https://pokemon-app-alpha-one.vercel.app",
+    technologies: ["React", "Tailwind CSS", "PokeApi", "Vercel"],
+  },
+];
