8e20610d217b695c4d4cb32500583dcd7d61a835
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2026-01-27 17:47:52 -0500

    feat: add favicon to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/icon-portfolio.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/icon-portfolio.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/icon-portfolio.png',
+        type: 'image/png',
       },
     ],
     apple: '/apple-icon.png',
